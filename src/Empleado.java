134d9c87fe5acc3c0e018705f3c5d6076a456707
ernyrodriguez <ernyrm2911@gmail.com>
2023-10-04 07:52:29 -0500

    subiendo class empleado

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,2 +1,58 @@
 public class Empleado {
+
+    String nombre;
+    String apellido;
+    int sueldo;
+    int contacto;
+    int fechaDeContratacion;
+
+    //constructor
+
+    public Empleado() {
+    }
+
+
+    //metodos get/set
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(int contacto) {
+        this.contacto = contacto;
+    }
+
+    public int getFechaDeContratacion() {
+        return fechaDeContratacion;
+    }
+
+    public void setFechaDeContratacion(int fechaDeContratacion) {
+        this.fechaDeContratacion = fechaDeContratacion;
+    }
+
 }
+
