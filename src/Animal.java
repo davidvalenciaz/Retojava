97fdbe0f63019acebb680f5dd2067ee66338644f
deiberjerez <deiber22cora@gmail.com>
2023-10-04 07:22:54 -0500

    vaciando el constructor y argregando gets and sets

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,4 @@
-public class Animal{
+public class Animal {
     //atributos
     String species;
     String race;
@@ -9,17 +9,65 @@ public class Animal{
     int price;
 
     //metodo constructor
-    Animal(String name, String species, String race, int age, String healthCondition,String description,int price){
-        this.name = name;
+    Animal() {
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
         this.species = species;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
         this.race = race;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getHealthCondition() {
+        return healthCondition;
+    }
+
+    public void setHealthCondition(String healthCondition) {
         this.healthCondition = healthCondition;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
         this.price = price;
     }
-    //metodos
-    public void felices(){
-        System.out.println("el " + species + " esta feliz");
-    }
+    //create
+    public void create(){}
+
 }
