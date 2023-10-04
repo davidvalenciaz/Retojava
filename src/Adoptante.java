91f44fc02bdd28936f6c02c98416950859b464c1
jeissss <jeisongomez03@gmail.com>
2023-10-04 07:19:05 -0500

    Commit 1 Jeison

--- src/Adoptante.java
+++ src/Adoptante.java
@@ -1,2 +1,55 @@
 public class Adoptante {
+
+    private String name;
+
+    private String adress;
+
+    private String contact;
+
+    private String preference;
+
+    private boolean confirm;
+
+    public Adoptante() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
+
+    public boolean isConfirm() {
+        return confirm;
+    }
+
+    public void setConfirm(boolean confirm) {
+        this.confirm = confirm;
+    }
 }
