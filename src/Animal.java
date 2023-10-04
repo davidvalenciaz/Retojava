2f0e555a1b44f28d7f148ea38f8c0bfd14122c77
deiberjerez <deiber22cora@gmail.com>
2023-10-04 06:32:45 -0500

    mi primer commit, atributos, metodo constructtor y metodos normales a Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,25 @@
-public class Animal {
+public class Animal{
+    //atributos
+    String species;
+    String race;
+    String name;
+    int age;
+    String healthCondition;
+    String description;
+    int price;
+
+    //metodo constructor
+    Animal(String name, String species, String race, int age, String healthCondition,String description,int price){
+        this.name = name;
+        this.species = species;
+        this.race = race;
+        this.age = age;
+        this.healthCondition = healthCondition;
+        this.description = description;
+        this.price = price;
+    }
+    //metodos
+    public void felices(){
+        System.out.println("el " + species + " esta feliz");
+    }
 }
